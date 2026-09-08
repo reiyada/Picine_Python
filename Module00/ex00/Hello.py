16c8bc045b42d6c69283969681020d502ded62f1
rei_qannt <reiyada0823@gmail.com>
2026-09-08 10:23:06 +0200

    00-00 done

---
+++ Module00/ex00/Hello.py
@@ -0,0 +1,23 @@
+ft_list = ["Hello"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
+
+#My code---------------
+
+ft_list.append("World!")
+
+tuple_france = ("France!",)
+ft_tuple = (ft_tuple[0],  (tuple_france[0]))
+
+ft_set.remove("tutu!")
+ft_set.add("Perpignan!")
+
+ft_dict.update({"Hello" : "42Perpignan!"})
+#----------------------
+
+# print(ft_tuple[:1])
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)

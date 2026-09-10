ba0e0ed17187a7bb03e0a7900c715842cb8f8c92
rei_qannt <reiyada0823@gmail.com>
2026-09-10 14:46:09 +0200

    01ex00 done

--- Module01/ex00/give_bmi.py
+++ Module01/ex00/give_bmi.py
@@ -1,18 +1,23 @@
-def parse_height_and_weight(height: list[int | float], weight: list[int | float]):
+def parse_height_and_weight(height: list[int | float],
+                            weight: list[int | float]):
     """This parse the given values and flag erros if needed"""
 
     for h in height:
-        assert isinstance(h, int) or isinstance(h, float), "Height needs to be int or float."
+        assert isinstance(h, int) or isinstance(h, float), \
+            "Height needs to be int or float."
     for w in weight:
-            assert isinstance(w, int) or isinstance(w, float), "Weight needs to be int or float."
-    assert len(height) == len(weight), "2 lists need to have the same amout of values."
+        assert isinstance(w, int) or isinstance(w, float), \
+            "Weight needs to be int or float."
+    assert len(height) == len(weight), \
+        "2 lists need to have the same amout of values."
     for h in height:
         assert h > 0, "Height needs to be more than 0."
     for w in weight:
         assert w > 0, "Weight needs to be more than 0."
 
 
-def calcul_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+def calcul_bmi(height: list[int | float],
+               weight: list[int | float]) -> list[int | float]:
     """This calculate bmis based on the given values and
         returns the list of them"""
 
@@ -25,7 +30,8 @@ def calcul_bmi(height: list[int | float], weight: list[int | float]) -> list[int
     return bmis
 
 
-def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+def give_bmi(height: list[int | float],
+             weight: list[int | float]) -> list[int | float]:
     """This returns the list of bmis"""
 
     try:
@@ -43,7 +49,7 @@ def parse_limit(limit: int):
 def check_limit(bmi: list[int | float], limit: int) -> list[bool]:
     """This checks if the given mbis are greater than the given limit,
             and it returns the list of boolean."""
-    
+
     limit_list = list()
     for b in bmi:
         limit_list.append(b > limit)
